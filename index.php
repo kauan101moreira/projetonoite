46b293cc6b1bdb779f3b6e41e103b714e7a40b02
igorluigi2003 <kauan.r.b.moreira@gmail.com>
2022-11-28 21:58:51 -0300

    Otimização do código das questões

--- index.php
+++ index.php
@@ -11,35 +11,34 @@ $pontos = 0;
 ?>
     <form action="./index.php" method="POST">
 
-<?php
-while($linha = mysqli_fetch_array($resultado)){
-    ?>
-       <div style="margin: 10px; width:100%; border:1px solid;">
-            <h1> <?php echo $linha["pergunta"]; ?> </h1>
-            <input type="radio" name="<?php echo $linha["id"]; ?>" value="A" value="a" />
-            <label>A) <?php echo $linha["a"]; ?> </label>
-            <br><br>
-            <input type="radio" name="<?php echo $linha["id"]; ?>" value="B" value="b" />
-            <label>B) <?php echo $linha["b"]; ?> </label>
-            <br><br>
-            <input type="radio" name="<?php echo $linha["id"]; ?>" value="C" value="c" />
-            <label>C) <?php echo $linha["c"]; ?> </label>
-            <br><br>
-            <input type="radio" name="<?php echo $linha["id"]; ?>" value="D" value="d" />
-            <label>D) <?php echo $linha["d"]; ?> </label>
-            <br><br>
-            <input type="radio" name="<?php echo $linha["id"]; ?>" value="E" value="e" />
-            <label>E) <?php echo $linha["e"]; ?> </label>
-            <br><br>
+        <?php
+        while($linha = mysqli_fetch_array($resultado)){
+            ?>
+            <div style="margin: 10px; width:100%; border:1px solid;">
+                <div class="offset-3 col-7">
+                        <div class="card-header">
+                            <h1> <?php echo $linha["pergunta"]; ?> </h1>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <h5><input type="radio" name="<?php echo $linha["id"]; ?>" value="A" value="a" />A) <?php echo $linha["a"]; ?> </h5>
+                                <h5><input type="radio" name="<?php echo $linha["id"]; ?>" value="B" value="b" />B) <?php echo $linha["b"]; ?> </h5>
+                                <h5><input type="radio" name="<?php echo $linha["id"]; ?>" value="C" value="c" />C) <?php echo $linha["c"]; ?> </h5>
+                                <h5><input type="radio" name="<?php echo $linha["id"]; ?>" value="D" value="d" />D) <?php echo $linha["d"]; ?> </h5>
+                                <h5><input type="radio" name="<?php echo $linha["id"]; ?>" value="E" value="e" />E) <?php echo $linha["e"]; ?> </h5>
+                                <br><br>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            <?php
+        }
+        ?>
+        <div class="offset">
+            <button class="btn btn-success" type="submit">Enviar respostas</button>
         </div>
-    <?php
-}
-?>
-
-        <button type="submit">Enviar respostas</button>
     </form>
     <br><br>
-
 <?php
 echo "Pontos: $pontos";
 include "rodape.php";
